38134f1db23b55d4377bf9b4216e78e5d8504289
rahmahaisyah <rahmahasyh@gmail.com>
2025-11-05 05:26:51 +0700

    feat: information

--- script/load-navbar.js
+++ script/load-navbar.js
@@ -2,8 +2,6 @@ fetch('navbar_pengelola.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('navbar-placeholder').innerHTML = data;
-    
-    // Set active menu berdasarkan halaman saat ini
     const currentPage = window.location.pathname.split('/').pop();
     const navItems = document.querySelectorAll('.nav-item');
     
